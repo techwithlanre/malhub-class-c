d0f89e254259eaa26ba076dc8d96460993f9f5aa
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2024-02-19 11:33:39 +0100

    init

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+function add(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+
+const name = function() {};
+
+
+const showAge = function () {
+    return 21;
+}
+
+function greetings() {
+    let name = "Jamiu";
+    let timeOfTheDay = "morning";
+    return "Good " + timeOfTheDay + ", " + name;
+}
