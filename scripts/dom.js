6f245317d4c6a0a68c6fea04687ac734610f143c
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2024-03-12 11:00:05 +0100

    eventd

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,3 +1,25 @@
-console.log(document.title = "Testing");
-console.log(document.title = "Testing");
+/* const h1 = document.getElementsByTagName('h1');
+console.log(h1[0].textContent);
+console.log(h1[1].innerHTML) */
+
+
+
+const button  = document.querySelector('#submitButton');
+const body  = document.querySelector('body');
+var firstName  = document.querySelector('#firstName');
+var lastName  = document.querySelector('#lastName');
+/* button.addEventListener("click", (event) => {
+    event.preventDefault();
+    firstName.value = "My name is jamiu";
+    lastName.value = "";
+}); */
+
+body.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target);  //button
+    console.log(event.currentTarget); // body
+});
+
+
+
 
